a2d18c24b854d3ee963195f0b6a2f9591dbd8fc4
Juan Vargas <juandeveloper19@gmail.com>
2023-01-30 12:19:37 -0500

    [FEAT] Centralized query
    A method was created to centralize the future methods that will consume the API

--- src/app/services/spotify-api.service.ts
+++ src/app/services/spotify-api.service.ts
@@ -8,20 +8,22 @@ import { map } from 'rxjs';
 export class SpotifyApiService {
   constructor(private http: HttpClient) {}
 
-  public getNewAlbumReleases() {
+  query(queryParams: string) {
+    const rootUrl = 'https://api.spotify.com/v1/';
+
     const headers: HttpHeaders = new HttpHeaders({
       Authorization:
         'Bearer BQB4zHMfnVW8TtHnXkscn1z5pjiX2D9aRKnbu-6LYlK4IYnnCqGr_w-YfOQjuJCJE-QbXNc7v-nGoOo4ucVHed3amCWQ8l1woBFLez1IjG0jjeuZ8I2j',
     });
 
-    return this.http
-      .get(`https://api.spotify.com/v1/browse/new-releases?limit=21`, {
-        headers,
+    return this.http.get(rootUrl + queryParams, { headers });
+  }
+
+  public getNewAlbumReleases() {
+    return this.query('browse/new-releases?limit=21').pipe(
+      map((data: any) => {
+        return data.albums.items;
       })
-      .pipe(
-        map((data: any) => {
-          return data.albums.items;
-        })
-      );
+    );
   }
 }
