dd3757e6e6a459bc5edd6d2a3aa7415c2c0235f1
Juan Vargas <juandeveloper19@gmail.com>
2023-01-30 08:18:44 -0500

    [FEAT] Navigation menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { LayoutComponent } from './ui/layout/layout.component';
 import { HomeComponent } from './ui/pages/home/home.component';
+import { NavbarComponent } from './ui/shared/navbar/navbar.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: NavbarComponent, outlet: 'navbar' },
+      { path: '', component: HomeComponent },
+    ],
   },
 ];
 
--- src/app/ui/components.module.ts
+++ src/app/ui/components.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 // All Components
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
-  declarations: [LayoutComponent, HomeComponent],
+  declarations: [LayoutComponent, HomeComponent, NavbarComponent],
   imports: [BrowserModule, CommonModule, RouterModule],
 })
 export class ComponentsModule {}
