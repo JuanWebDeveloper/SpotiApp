99c419a2c95c27e21ad3e9d87fba5ccab17ef0b8
Juan Vargas <juandeveloper19@gmail.com>
2023-01-30 06:20:38 -0500

    [FEAT] Initial application setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'spoti-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SpotiApp';
-}
+export class AppComponent {}
